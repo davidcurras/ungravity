cf75022accdef02e1e949b5815e1430d25b12cb3
davidcurras <davidc.ferre@gmail.com>
2013-07-03 18:08:42 -0300

    Real Options added

--- scenes/Options.js
+++ scenes/Options.js
@@ -11,12 +11,12 @@ ungravity.scenes.Options = function() {
     this.layer = new lime.Layer();
     this.createLabels();
     this.appendChild(this.layer);
-    goog.events.listen(this, ['mousedown', 'touchend'], this.clickHandler);
 };
 
 goog.inherits(ungravity.scenes.Options, lime.Scene);
 
 goog.object.extend(ungravity.scenes.Options.prototype, {
+
     /**
      * The Options scene layer
      * @type {lime.Layer}
@@ -28,26 +28,37 @@ goog.object.extend(ungravity.scenes.Options.prototype, {
      * @return {undefined} Nothing returned
      */
     createLabels: function() {
-        var defaultLabelHeight = 50;
-        var optionLines = [
-            'Sounds',
-            'Graphics',
-            'Controls',
-            'Back'
-        ];
-        for(var i in optionLines){
-            var option = optionLines[i];
-            var xPos = ungravity.settings.width/2;
-            var yPos = (ungravity.settings.height/2) - ((defaultLabelHeight/2)*optionLines.length) + (defaultLabelHeight*i);
-            var label = new lime.Label()
-                .setText(option)
-                .setFontFamily('Quantico')
-                .setFontSize(20)
-                .setFontColor('#ffffff')
-                .setAnchorPoint(0.5, 0.5)
-                .setPosition(xPos, yPos);
-            this.layer.appendChild(label);
-        }
+        var Lbl = ungravity.scenes.Options.Labels;
+        for (var key in Lbl) {
+            Lbl[key].label = new lime.Label()
+                .setFontFamily('Permanent Marker')
+                .setFontSize(30)
+                .setFontColor('#FFF')
+                .setAnchorPoint(0, 0);
+            if(typeof Lbl[key].eventHandler !== 'undefined'){
+                goog.events.listen(Lbl[key].label, ['mousedown', 'touchend'], Lbl[key].eventHandler);
+            }
+            this.layer.appendChild(Lbl[key].label);
+        };
+        Lbl.Options.label.setText('Options')
+            .setFontSize(50)
+            .setAnchorPoint(0.5, 0)
+            .setPosition(ungravity.settings.width/2, 25);
+        Lbl.Sound.label.setText('Sound')
+            .setPosition(200, 130);
+        var onOffText = ungravity.settings.isMuted ? 'Off' : 'On';
+        Lbl.OnOff.label.setText(onOffText)
+            .setFontFamily('Quantico')
+            .setPosition(400, 130);
+        Lbl.Graphics.label.setText('Graphics')
+            .setPosition(200, 230);
+        var canvasOnOffText = ungravity.settings.canvasRenderer ? 'Canvas Renderer' : 'HTML Elements Renderer';
+        Lbl.CanvasOnOff.label.setText(canvasOnOffText)
+            .setFontFamily('Quantico')
+            .setPosition(400, 230);
+        Lbl.Back.label.setText('Back')
+            .setAnchorPoint(0.5, 0)
+            .setPosition(ungravity.settings.width/2, 330);
     },
 
     /**
@@ -58,3 +69,50 @@ goog.object.extend(ungravity.scenes.Options.prototype, {
         ungravity.director.replaceScene(new ungravity.scenes.Menu(), lime.transitions.Dissolve);
     }
 });
+
+/**
+ * The event handler for Sound option click
+ * @return {undefined} Nothing returned
+ */
+ungravity.scenes.Options.ToggleSound = function(){ 
+    var soundOnOff = ungravity.scenes.Options.Labels.OnOff;
+    if(ungravity.settings.isMuted){
+        soundOnOff.label.setText('On');
+        lime.audio.setMute(false);
+        ungravity.settings.isMuted = false;
+    } else {
+        soundOnOff.label.setText('Off');
+        lime.audio.setMute(true);
+        ungravity.settings.isMuted = true;
+    }
+};
+
+/**
+ * The event handler for Graphics option click
+ * @return {undefined} Nothing returned
+ */
+ungravity.scenes.Options.ToggleGraphics = function(){
+    var canvasOnOff = ungravity.scenes.Options.Labels.CanvasOnOff;
+    if(ungravity.settings.canvasRenderer){
+        canvasOnOff.label.setText('HTML Elements Renderer');
+        ungravity.settings.canvasRenderer = false;
+    } else {
+        canvasOnOff.label.setText('Canvas Renderer');
+        ungravity.settings.canvasRenderer = true;
+    }
+};
+
+/**
+ * A hash containing the option labels
+ * @type {Object}
+ */
+ungravity.scenes.Options.Labels = {
+    'Options': {'label':undefined , 'eventHandler':undefined},
+    'Sound': {'label':undefined , 'eventHandler':undefined},
+    'OnOff': {'label':undefined , 'eventHandler':ungravity.scenes.Options.ToggleSound},
+    'Graphics': {'label':undefined , 'eventHandler':undefined},
+    'CanvasOnOff': {'label':undefined , 'eventHandler':ungravity.scenes.Options.ToggleGraphics},
+    'Back': {'label':undefined , 'eventHandler':function() {
+        ungravity.director.replaceScene(new ungravity.scenes.Menu(), lime.transitions.Dissolve);
+    }}
+};
